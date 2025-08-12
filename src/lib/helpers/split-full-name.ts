88e4547a62f9a50707ef3a4d938f9b3c1169b1cc
ARIJIT <arijitchhatui.me@gmail.com>
2025-08-12 22:44:43 +0530

    fix: postAssets and assets and getPosts endpoint (#34)

--- src/lib/helpers/split-full-name.ts
+++ src/lib/helpers/split-full-name.ts
@@ -1,4 +1,7 @@
 import { REMOVE_SPACE_REGEX } from '../../services/auth';
+import { GetPostsOutput } from '../../services/posts';
+import { AssetsEntity } from '../../services/rdb/entities';
+import { PostTypes } from '../../services/service.constants';
 
 export const splitFullName = (fullName: string): { firstName: string; lastName: string } => {
   const nameParts = fullName.trim().split(REMOVE_SPACE_REGEX);
@@ -8,4 +11,33 @@ export const splitFullName = (fullName: string): { firstName: string; lastName: 
   const lastName = (hasLastName ? nameParts[nameParts.length - 1] : null) || '';
 
   return { firstName, lastName };
+};
+
+type GetPostsOutputType = {
+  id: string;
+  caption: string;
+  creatorId: string;
+  unlockPrice: number | null;
+  likeCount: number;
+  saveCount: number;
+  shareCount: number;
+  commentCount: number;
+  totalEarning: number;
+  types: PostTypes[];
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+  lastCommentId: string;
+  assets: AssetsEntity;
+};
+
+export const getPosts = () => {
+  const rawPosts: GetPostsOutputType[] = [];
+
+  const postsMap: Record<string, GetPostsOutput> = {};
+  for (const rawPost of rawPosts) {
+    postsMap[rawPost.id] ??= { ...rawPost, assets: [] };
+    postsMap[rawPost.id].assets.push(rawPost.assets);
+  }
+  return Object.values(postsMap);
 };
