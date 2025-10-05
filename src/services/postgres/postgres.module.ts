859b6695014bfcf6dfaa2f19a8c8623993c5bdea
ARIJIT <arijitchhatui.me@gmail.com>
2025-10-05 12:50:58 +0530

    feat: migrate data to railway

--- src/services/postgres/postgres.module.ts
+++ src/services/postgres/postgres.module.ts
@@ -1,9 +1,9 @@
+import { EntityMaker } from '@app/methods';
 import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import * as repositories from './repositories';
-import { EntityMaker } from '@app/methods';
 
 @Global()
 @Module({
@@ -14,7 +14,7 @@ import { EntityMaker } from '@app/methods';
       useFactory: (configService: ConfigService) => {
         const options: TypeOrmModuleOptions = {
           type: 'postgres',
-          url: configService.get('SUPABASE_DB_URL'),
+          url: configService.get('RAILWAY_DB_URL'),
           entities: [__dirname + '/../**/entities/**/*.entity.js'],
           migrations: [__dirname + '/../**/migrations/**/*[!index].js'],
           migrationsRun: !!configService.get('RUN_DB_MIGRATIONS_ON_START'),
